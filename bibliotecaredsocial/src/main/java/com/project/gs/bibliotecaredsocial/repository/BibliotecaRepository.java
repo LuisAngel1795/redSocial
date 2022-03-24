c5e3f19c2acf8ee23b2565218d0373714ab2655b
Luis Angel Silva <16030272@itcelaya.edu.mx>
2022-03-23 18:24:32 -0600

    version funcional con mensaje funcional

--- bibliotecaredsocial/src/main/java/com/project/gs/bibliotecaredsocial/repository/BibliotecaRepository.java
+++ bibliotecaredsocial/src/main/java/com/project/gs/bibliotecaredsocial/repository/BibliotecaRepository.java
@@ -5,5 +5,5 @@ import com.project.gs.bibliotecaredsocial.entity.Biblioteca;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BibliotecaRepository extends CrudRepository<Biblioteca,Long>{
-    
+    //esta es la ultima version
 }
